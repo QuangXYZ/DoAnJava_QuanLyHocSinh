71932b8dc8470e728fe1bbd8bce1ccf69123880a
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2022-04-09 09:32:54 +0700

    class quản lí điểm học sinh

--- src/quanlyhocsinh/HocSinh.java
+++ src/quanlyhocsinh/HocSinh.java
@@ -7,7 +7,6 @@ public class HocSinh extends Nguoi {
 	private String MSHS;
 	private String lop;
 	private String img;
-	private double toan, van, anh, hoa, ly, sinh;
 	
 	public HocSinh() {
 		
@@ -15,17 +14,12 @@ public class HocSinh extends Nguoi {
 	
 	
 	
-	public HocSinh(String mSHS,String hoTen, Date ngaySinh,String gioiTinh, String queQuan, String lop,String img, double toan, double van, double anh, double hoa, double sinh, double ly) {
+	public HocSinh(String mSHS,String hoTen, Date ngaySinh,String gioiTinh, String queQuan, String lop,String img) {
 		super(hoTen, gioiTinh, queQuan, ngaySinh);
 		this.MSHS = mSHS;
 		this.lop = lop;
 		this.img = img;
-		this.toan = toan;
-		this.van = van;
-		this.anh = anh;
-		this.hoa = hoa;
-		this.ly = ly;
-		this.sinh = sinh;
+		
 	}
 
 
@@ -38,47 +32,6 @@ public class HocSinh extends Nguoi {
 		this.img = img;
 	}
 	
-	public double getToan() {
-		return toan;
-	}
-	public void setToan(double toan) {
-		this.toan = toan;
-	}
-
-	public double getVan() {
-		return van;
-	}
-	public void setVan(double van) {
-		this.van = van;
-	}
-
-	public double getAnh() {
-		return anh;
-	}
-	public void setAnh(double anh) {
-		this.anh = anh;
-	}
-
-	public double getHoa() {
-		return hoa;
-	}
-	public void setHoa(double hoa) {
-		this.hoa = hoa;
-	}
-
-	public double getLy() {
-		return ly;
-	}
-	public void setLy(double ly) {
-		this.ly = ly;
-	}
-
-	public double getSinh() {
-		return sinh;
-	}
-	public void setSinh(double sinh) {
-		this.sinh = sinh;
-	}
 
 	public String getMSHS() {
 		return MSHS;
---
+++ src/quanlyhocsinh/QuanLiDiemHS.java
@@ -0,0 +1,71 @@
+package quanlyhocsinh;
+
+public class QuanLiDiemHS {
+	private double toan, anh, van, ly, sinh, hoa;
+
+	public QuanLiDiemHS() {
+		
+	}
+
+	public QuanLiDiemHS(double toan, double anh, double van, double ly, double sinh, double hoa) {
+		super();
+		this.toan = toan;
+		this.anh = anh;
+		this.van = van;
+		this.ly = ly;
+		this.sinh = sinh;
+		this.hoa = hoa;
+	}
+
+	public double getToan() {
+		return toan;
+	}
+
+	public void setToan(double toan) {
+		this.toan = toan;
+	}
+
+	public double getAnh() {
+		return anh;
+	}
+
+	public void setAnh(double anh) {
+		this.anh = anh;
+	}
+
+	public double getVan() {
+		return van;
+	}
+
+	public void setVan(double van) {
+		this.van = van;
+	}
+
+	public double getLy() {
+		return ly;
+	}
+
+	public void setLy(double ly) {
+		this.ly = ly;
+	}
+
+	public double getSinh() {
+		return sinh;
+	}
+
+	public void setSinh(double sinh) {
+		this.sinh = sinh;
+	}
+
+	public double getHoa() {
+		return hoa;
+	}
+
+	public void setHoa(double hoa) {
+		this.hoa = hoa;
+	}
+	
+	
+	
+
+}
