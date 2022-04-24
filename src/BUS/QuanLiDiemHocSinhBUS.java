286d445eeb41bd94f9c2dbc56aa798cc49fe5846
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2022-04-24 10:40:42 +0700

    Tạo lớp QuanLiDiemHocSinhBUS: gọi lại các phương thức trong QuanLiDiemHocSinh DAO

---
+++ src/BUS/QuanLiDiemHocSinhBUS.java
@@ -0,0 +1,28 @@
+package BUS;
+
+import DAO.QuanLyDiemHocSinhDAO;
+import DTO.QuanLiDiemHS;
+import java.util.ArrayList;
+
+public class QuanLiDiemHocSinhBUS {
+	QuanLyDiemHocSinhDAO diemHocSinhDAO = new QuanLyDiemHocSinhDAO();
+	public ArrayList<QuanLiDiemHS> docDiemHocSinh(){
+		return diemHocSinhDAO.docDiemHocSinh();
+	}
+	
+	public int themDiemHocSinh(QuanLiDiemHS diemHS) {
+		return diemHocSinhDAO.themDiemHocSinh(diemHS);
+	}
+	
+	public int xoaDiemHocSinh(QuanLiDiemHS diemHS) {
+		return diemHocSinhDAO.xoaDiemHocSinh(diemHS);
+	}
+	
+	public int suaDiemHocSinh(QuanLiDiemHS diemHS) {
+		return diemHocSinhDAO.suaDiemHocSinh(diemHS);
+	}
+	
+	public ArrayList<QuanLiDiemHS> timDiemHocSinh(String mshs){
+		return diemHocSinhDAO.timDiemHocSinh(mshs);
+	}
+}
